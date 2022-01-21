6a24f66577717be105f40762c7754d2b024b13e7
Victor Geraldsson <victor@geraldsson.org>
2022-01-21 11:32:20 +0100

    Revert "Make sure cart changes are shown on checkout"
    This reverts commit 3782617128fb445091bbfc663f99dc5d32fe25c0.

--- Magento_Checkout/web/js/view/minicart.js
+++ Magento_Checkout/web/js/view/minicart.js
@@ -107,10 +107,6 @@ define([
                 openModal();
             } else if (location.pathname === '/sveacheckout/') {
                 minicart.load();
-                minicart.changedItems.subscribe(function () {
-                    minicartContent.modal('option').isOpen ||
-                        minicartContent.modal('openModal');
-                });
             }
 
             function openModal() {
