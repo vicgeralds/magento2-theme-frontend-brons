afd66457096f4594ae9685e265361ec81a374677
Victor Geraldsson <victor@geraldsson.org>
2023-11-21 23:14:54 +0100

    Add search

--- Magento_Theme/web/js/theme.js
+++ Magento_Theme/web/js/theme.js
@@ -4,10 +4,20 @@
  */
 
 define([
+    'jquery',
     'mage/smart-keyboard-handler',
     'domReady!'
-], function (keyboardHandler) {
+], function ($, keyboardHandler) {
     'use strict';
+
+    var header = $('.page-header')
+    header.find('.minisearch .input-text')
+        .focus(function () {
+            header.addClass('search-focus')
+        })
+        .blur(function () {
+            header.removeClass('search-focus')
+        })
 
     keyboardHandler.apply();
 });
