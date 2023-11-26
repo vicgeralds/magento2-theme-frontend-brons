c1263f1a9940992df0f53cb277d4648acae81620
Victor Geraldsson <victor@geraldsson.org>
2023-11-26 18:35:08 +0100

    Avoid pusing history entry when opening/closing minicart

--- Magento_Checkout/web/js/view/minicart.js
+++ Magento_Checkout/web/js/view/minicart.js
@@ -51,6 +51,8 @@ define([
             });
             $('[data-block="minicart"]').on('contentLoading', function () {
                 addToCartCalls++;
+            }).on('click', function (e) {
+                e.preventDefault()
             });
 
             var showcart = '#showcart';
@@ -73,8 +75,8 @@ define([
 
                     if (window.location.hash === showcart) {
                         var history = window.history;
-                        if (history && history.pushState) {
-                            history.pushState(null, document.title, location.href.split('#')[0]);
+                        if (history && history.replaceState) {
+                            history.replaceState(null, '', location.href.split('#')[0]);
                         }
                     }
 
