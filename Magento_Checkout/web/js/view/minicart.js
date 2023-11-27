51a4b1e8f3c2d4cdb9369cb817aa61b47d35841e
Victor Geraldsson <victor@geraldsson.org>
2023-11-27 01:04:31 +0100

    Fix opening minicart

--- Magento_Checkout/web/js/view/minicart.js
+++ Magento_Checkout/web/js/view/minicart.js
@@ -49,8 +49,14 @@ define([
             });
             $('[data-block="minicart"]').on('contentLoading', function () {
                 addToCartCalls++;
-            }).on('click', function (e) {
-                e.preventDefault()
+            });
+
+            var history = window.history;
+            $('a.action.showcart').on('click', function (e) {
+                if (history && history.replaceState) {
+                    e.preventDefault();
+                    minicart.load();
+                }
             });
 
             var showcart = '#showcart';
@@ -72,7 +78,6 @@ define([
                     });
 
                     if (window.location.hash === showcart) {
-                        var history = window.history;
                         if (history && history.replaceState) {
                             history.replaceState(null, '', location.href.split('#')[0]);
                         }
